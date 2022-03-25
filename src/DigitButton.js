9be0b50457dd61ae32d56bb6ee3414896734c5d8
Sarah <sarah.bocognano@gmail.com>
2022-03-25 17:52:55 +0100

    new version

---
+++ src/DigitButton.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./App";
+import { useDispatch } from "react-redux";
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
